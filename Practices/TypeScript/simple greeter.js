e7da17105ce80ef484775b9be8c2818269dfadc0
José Herazo <100380601@alumnos.uc3m.es>
2018-03-15 22:37:51 +0100

    Folder for Practices

---
+++ Practices/TypeScript/simple greeter.js
@@ -0,0 +1,6 @@
+//Programa muy simple para probar la traducción a javascript
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "Juan";
+document.body.innerHTML = greeter(user);
---
+++ Practices/TypeScript/simple greeter.ts
@@ -0,0 +1,7 @@
+//Programa muy simple para probar la traducción a javascript
+function greeter (person){
+    return "Hello, "+person;
+}
+
+let user="Juan";
+document.body.innerHTML=greeter(user);
