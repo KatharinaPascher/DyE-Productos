a6fce3aeed083e44e99c8f9a04bd0f1d90fcb01a
Katharina Pascher <a1246809@unet.univie.ac.at>
2018-04-10 16:06:52 +0200

    Initial commit

---
+++ Cibo/src/app/app.module.ts
@@ -0,0 +1,54 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+
+import { HomePage } from '../pages/home/home';
+import {FavsPage} from '../pages/favs/favs';
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { SettingsPage } from '../pages/settings/settings';
+import { DistancePage } from '../pages/distance/distance';
+import { AllergiesPage } from '../pages/allergies/allergies';
+import { PricePage } from '../pages/price/price';
+import { FoodPage } from '../pages/food/food';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    SettingsPage,
+    FavsPage,
+    HomePage,
+    TabsPage,
+    DistancePage,
+    AllergiesPage,
+    PricePage,
+    FoodPage
+
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    SettingsPage,
+    FavsPage,
+    HomePage,
+    TabsPage,
+    DistancePage,
+    AllergiesPage,
+    PricePage,
+    FoodPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
